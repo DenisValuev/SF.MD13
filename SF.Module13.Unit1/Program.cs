1811b125b9009179cc29bd8344a068a48248e334
DenisValuev <valuev.denis91@mail.ru>
2024-10-07 08:44:29 +0300

    Добавьте файлы проекта.

---
+++ SF.Module13.Unit1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace SF.Module13.Unit1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CheckAscending(new[] { -1, 2, 3, 4, 8 }));
+            Console.WriteLine(CheckAscending(new[] { -1, 2, 3, 10, 8 }));
+        }
+        static bool CheckAscending(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
